a605d7f58a41b992eab8f5e582cf929a6b73149a
Matias Paloranta <paloranta.matias@gmail.com>
2024-02-22 23:54:45 +0200

    Ex 3.5 and 3.6

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     { 
       "id": 1,
@@ -52,6 +54,32 @@ app.delete('/api/persons/:id', (request, response) => {
     persons = persons.filter(person => person.id !== id)
 
     response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name) {
+        return response.status(400).json({error: 'name missing'})
+    }
+
+    if (!body.number) {
+        return response.status(400).json({error: 'number missing'})
+    }
+
+    if (persons.some(person => JSON.stringify(person.name) === JSON.stringify(body.name))) {
+        return response.status(400).json({error: 'name must be unique'})
+    }
+
+    const person = {
+        id: Math.round(Math.random()*10000000),
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
 })
 
 const PORT = 3001
