b954638c57cd81b8260105fcf82db60f56ef7dad
Matias Paloranta <paloranta.matias@gmail.com>
2024-03-05 21:26:50 +0200

    Ex 3.16

--- index.js
+++ index.js
@@ -57,33 +57,31 @@ app.get('/api/persons', (request, response) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id)
     .then(person => {
         if (person) {
             response.json(person)
         } else {
-            response.status(404).end()
+            response.status(404).send({ error: 'unknown endpoint' })
         }
     })
     .catch(error => {
-        console.log(error)
-        response.status(400).send({ error: 'malformatted id' })
+        next(error)
     })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndDelete(request.params.id)
     .then(result => {
         response.status(204).end()
     })
     .catch(error => {
-        console.log(error)
-        response.status(404).send({ error: 'id not found' })
+        next(error)
     })
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     if (!body.name) {
@@ -109,12 +107,12 @@ app.post('/api/persons', (request, response) => {
         response.json(savedPerson)
     })
     .catch(error => {
-        console.log(error)
+        next(error)
     })
     
 })
 
-app.put('/api/persons/:id', (request, response) => {
+app.put('/api/persons/:id', (request, response, next) => {
     const body = request.body
 
     const person = {
@@ -127,10 +125,22 @@ app.put('/api/persons/:id', (request, response) => {
         response.json(updatedPerson)
     })
     .catch(error => {
-        console.log(error)
+        next(error)
     })
 
 })
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    }
+  
+    next(error)
+  }
+  
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
